0560e1357e371a2928df3ec9980bdd28893b9373
NovaJ115 <111328457+NovaJ115@users.noreply.github.com>
2022-09-23 22:58:34 -0500

    Small changes

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public GameObject spawnArea;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Its working");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log("Its working");
+
+    }
+}
