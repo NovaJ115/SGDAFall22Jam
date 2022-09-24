6303e9137b6f9a8f5b8d1408501a43587616aa10
Xernacus <a10rec@myfastmail.com>
2022-09-23 23:36:24 -0500

    Bullet Despawns
    yes

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+        //StartCoroutine(DestroBullet());
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+    /**
+    public IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(GameObject);
+    }
+    **/
+
+}
