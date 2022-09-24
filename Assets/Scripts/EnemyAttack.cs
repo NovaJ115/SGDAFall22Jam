d16e65c53cc93f26b78ee356481401d3adda7d18
JoshAltshuler <Josh.Altshuler1@gmail.com>
2022-09-24 13:47:10 -0500

    Random Fire Rate

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -6,7 +6,7 @@ public class EnemyAttack : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
 
-    public float fireRate = 1f;
+    private float fireRate = 3f;
     private float nextFire;
     public Transform enemy;
     public GameObject bulletCasing;
@@ -26,6 +26,7 @@ public class EnemyAttack : MonoBehaviour
             GameObject casing = Instantiate(bulletCasing, enemy.position, enemy.rotation);
             rb = casing.GetComponent<Rigidbody2D>();
             rb.AddForce(enemy.up * Random.Range(-1.0f, 1.0f), ForceMode2D.Impulse);
+            fireRate = Random.Range(2.7f, 3.3f);
             nextFire = Time.time + fireRate;
         }
         if (Time.time > (nextFire - (fireRate/2)) && rb != null)
